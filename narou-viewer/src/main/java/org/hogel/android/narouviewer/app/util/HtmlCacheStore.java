4417b52764a05eed1a86c3ac47042fd740c82853
hogelog <konbu.komuro@gmail.com>
2014-05-14 22:28:33 +0900

    Add slf4j-android

--- narou-viewer/src/main/java/org/hogel/android/narouviewer/app/util/HtmlCacheStore.java
+++ narou-viewer/src/main/java/org/hogel/android/narouviewer/app/util/HtmlCacheStore.java
@@ -3,16 +3,33 @@ package org.hogel.android.narouviewer.app.util;
 import com.google.common.base.Optional;
 import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.RemovalListener;
+import com.google.common.cache.RemovalNotification;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-public class HtmlCacheStore {
+public class HtmlCacheStore implements RemovalListener<Object, Object> {
+    private static final Logger LOG = LoggerFactory.getLogger(HtmlCacheStore.class);
+
     private final Cache<String, String> cache;
 
     public HtmlCacheStore() {
-        CacheBuilder<Object, Object> cacheBuilder = CacheBuilder.newBuilder().maximumSize(10).softValues();
+        CacheBuilder<Object, Object> cacheBuilder =
+                CacheBuilder
+                        .newBuilder()
+                        .maximumSize(10)
+                        .softValues()
+                        .removalListener(this);
         cache = cacheBuilder.build();
     }
 
+    @Override
+    public void onRemoval(RemovalNotification<Object, Object> notification) {
+        LOG.info("removed: {}", notification.getKey());
+    }
+
     public void addCache(String url, String data) {
+        LOG.info("cached: {}", url);
         cache.put(url, data);
     }
 
